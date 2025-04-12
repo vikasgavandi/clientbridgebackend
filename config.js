2556566c8bc7748e5fe5580b1dac501a0ead92a2
Vikas Gavandi <30316401+vikasgavandi@users.noreply.github.com>
2025-04-12 16:44:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: 4000,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
