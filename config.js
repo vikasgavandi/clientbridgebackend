73bbbb1289f72fb385a1187f4ee20f40d228c06c
Vikas Gavandi <30316401+vikasgavandi@users.noreply.github.com>
2025-04-12 17:33:19 +0530

    database configuration changes
    configuration variables changes

--- config.js
+++ config.js
@@ -1,3 +1,23 @@
+// const mysql = require('mysql2/promise');
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: 4000, // Default port for TiDB Cloud
+//   ssl: {
+//     minVersion: 'TLSv1.2',           // Enforce minimum TLS version
+//     rejectUnauthorized: false         // Validates server's SSL cert
+//   },
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+// module.exports = pool;
+
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
@@ -5,11 +25,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: 4000, // Default port for TiDB Cloud
-  ssl: {
-    minVersion: 'TLSv1.2',           // Enforce minimum TLS version
-    rejectUnauthorized: true         // Validates server's SSL cert
-  },
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
